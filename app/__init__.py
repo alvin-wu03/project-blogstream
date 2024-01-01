bf0592a067e0a4fa0bc89fd0e2a4f55b8378b9f6
Alvin Wu <60025632+alvin-wu03@users.noreply.github.com>
2023-12-31 20:07:02 -0500

    comments

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ db = sqlite3.connect(DB_FILE, check_same_thread = False)
 cursor = db.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS users(UserID INTEGER NOT NULL PRIMARY KEY, Username text NOT NULL UNIQUE, Password text, About text, JoinDate text);')
 cursor.execute('CREATE TABLE IF NOT EXISTS posts(PostID INTEGER NOT NULL PRIMARY KEY, UserID INTEGER NOT NULL, Title text NOT NULL, Text text, Date text, Likes INTEGER NOT NULL);')
+cursor.execute('CREATE TABLE IF NOT EXISTS comments(CommentID INTEGER NOT NULL PRIMARY KEY, PostID INTEGER NOT NULL, UserID INTEGER NOT NULL, Text text, Date text);')
 db.commit()
 
 @app.route("/")
@@ -85,7 +86,7 @@ def logout():
 
 @app.route("/new_post", methods=['POST'])
 def new_post():
-    if not request.form.get('title') or 'userID' not in session:
+    if not request.form.get('title') or 'userID' not in session or 'username' not in session:
         return render_template("error.html")
     else:
         cursor = db.cursor()
@@ -132,17 +133,48 @@ def delete_post():
         db.commit()
         return redirect("/dashboard")
 
+@app.route("/comment", methods=['POST'])
+def comment_post():
+    post_id = request.form.get('post_id')
+    user_id = request.form.get('user_id')
+    if not request.form.get('comment_text') or post_id == None or user_id == None or not 'userID' in session:
+        return render_template("error.html")
+    else:
+        cursor = db.cursor()
+        comment_text = html.escape(request.form.get('comment_text'))
+        date = datetime.now()
+        parameters = (post_id, session['userID'], comment_text, date)
+        cursor.execute('INSERT INTO comments(PostID, UserID, Text, Date) VALUES(?,?,?,?)', parameters)
+        db.commit()
+        return redirect(f"/post/{post_id}")
+
 @app.route("/post/<postid>", methods=['GET'])
 def get_post(postid):
-    cursor = db.cursor()
-    cursor.execute('SELECT UserID, Title, Text, Date, Likes FROM posts WHERE PostID=?', (postid,))
-    data = cursor.fetchone()
-    if data:
-        cursor.execute('SELECT Username FROM users WHERE UserID=?', (data[0],))
-        username = cursor.fetchone()
-        return render_template("post.html", post_id = postid, user_id = data[0], author=username[0], title=data[1], text=data[2], date=data[3], likes=data[4], logged_in = True)
+    if 'username' in session and 'userID' in session:
+        cursor = db.cursor()
+        cursor.execute('SELECT UserID, Title, Text, Date, Likes FROM posts WHERE PostID=?', (postid,))
+        data = cursor.fetchone()
+        cursor.execute('SELECT * FROM comments WHERE PostID=?', (postid,))
+        comments = cursor.fetchall()
+        if data:
+            cursor.execute('SELECT Username FROM users WHERE UserID=?', (data[0],))
+            userData = cursor.fetchone()
+            for i in range(len(comments)):
+                user_id = comments[i][1]  # Assuming UserID is at index 2 in comments tuple
+                cursor.execute('SELECT Username FROM users WHERE UserID=?', (user_id,))
+                result = cursor.fetchone()
+                if result:
+                    print(result)
+                    username = result[0]  # Assuming Username is the first column in users table
+                    comment_author = username
+                    # Append the username to each comment
+                    comments[i] = (comments[i], comment_author)
+            #print(comments)
+            return render_template("post.html", post_id = postid, user_id = data[0], author=userData[0], title=data[1], text=data[2], date=data[3], likes=data[4], comments = comments, logged_in = True)
+        else:
+            return render_template("error.html")
     else:
-        return render_template("error.html")
+        return redirect("/login")
     
 @app.route("/get_posts")
 def get_posts():
@@ -156,34 +188,42 @@ def get_posts():
     
 @app.route("/feed")
 def render_feed():
-    return render_template("feed.html", logged_in = True, posts = get_posts())
+    if 'username' in session and 'userID' in session:
+        return render_template("feed.html", logged_in = True, posts = get_posts())
+    else:
+        return redirect("/login")
 
 @app.route("/user/<userid>", methods=['GET'])
 def get_user(userid):
-    cursor = db.cursor()
-    cursor.execute('SELECT Username, JoinDate FROM users WHERE UserID=?', (userid,))
-    userData = cursor.fetchone()
-    if userData and 'userID' in session:
-        username = userData[0]
-        join_date = userData[1]
-        cursor.execute('SELECT * FROM posts WHERE UserID=?', (userid,))
-        posts = cursor.fetchall()
-        return render_template("user_profile.html", userid = userid, username = username, join_date = join_date, posts = posts[-10:][::-1], logged_in="True")
-    else:
-        return render_template("error.html")
+    if 'username' in session and 'userID' in session:
+        cursor = db.cursor()
+        cursor.execute('SELECT Username, JoinDate FROM users WHERE UserID=?', (userid,))
+        userData = cursor.fetchone()
+        if userData and 'userID' in session:
+            username = userData[0]
+            join_date = userData[1]
+            cursor.execute('SELECT * FROM posts WHERE UserID=?', (userid,))
+            posts = cursor.fetchall()
+            return render_template("user_profile.html", userid = userid, username = username, join_date = join_date, posts = posts[-5:][::-1], logged_in="True")
+        else:
+            return render_template("error.html")
+    return redirect("/login")
 
 @app.route("/user/<userid>/posts", methods=['GET'])
 def get_user_posts(userid):
-    cursor = db.cursor()
-    cursor.execute('SELECT Username FROM users WHERE UserID=?', (userid,))
-    userData = cursor.fetchone()
-    if userData and 'userID' in session:
-        username = userData[0]
-        cursor.execute('SELECT * FROM posts WHERE UserID=?', (userid,))
-        posts = cursor.fetchall()
-        return render_template("user_posts.html", username = username, posts = posts[::-1], logged_in="True")
+    if 'username' in session and 'userID' in session:
+        cursor = db.cursor()
+        cursor.execute('SELECT Username FROM users WHERE UserID=?', (userid,))
+        userData = cursor.fetchone()
+        if userData and 'userID' in session:
+            username = userData[0]
+            cursor.execute('SELECT * FROM posts WHERE UserID=?', (userid,))
+            posts = cursor.fetchall()
+            return render_template("user_posts.html", username = username, posts = posts[::-1], logged_in="True")
+        else:
+            return render_template("error.html")
     else:
-        return render_template("error.html")    
+        return redirect("/login")    
 
 @app.route("/self")    
 def get_self():
